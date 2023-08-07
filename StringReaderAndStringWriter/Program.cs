4502c897604252bb594ff20e2ab3eece9cd5555b
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:09:43 +0200

    Add project files.

---
+++ StringReaderAndStringWriter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace StringReaderAndStringWriter
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            StringReadWrite srw = new StringReadWrite();
+
+        }
+
+        #region
+         void StringReadWriter(string input)
+        {
+            WriteData();
+            ReadData();
+
+
+        }
+        #endregion
+        public void WriteData()
+        {
+
+            StringWriter sw = new StringWriter();
+            Console.WriteLine("olease enter first and last name...");
+            string name = Console.ReadLine();
+
+            sw.WriteLine("Name: "+ name);
+            sw.Flush();
+            sw.Close();
+
+        }
+       
+
+    }
+
+
+}
